5eecbdb8e4cbc84df0a5951b6c111242ca1cc467
Colin Millar <colin@ices.dk>
2017-02-14 17:04:24 +0100

    switch to using httr and xml2

--- R/utilities.R
+++ R/utilities.R
@@ -1,71 +1,48 @@
 
 
-#' @importFrom utils download.file
+#url <- httr::modify_url("https://",
+#                        host = "sg.ices.dk/manage/StockAssessmentGraphsWithToken.asmx", 
+#                        path = "getListAvailableSAGSettingsPerChart",
+#                        query = list(SAGChartKey = 1, token = SAG_pat()))
+
+#x <- readSAG(url)
+
 readSAG <- function(url) {
-  # try downloading first:
-  # create file name
-  tmp <- tempfile()
-  on.exit(unlink(tmp))
-
-  # download file
-  ret <-
-    if (os.type("windows")) {
-      download.file(url, destfile = tmp, quiet = TRUE)
-    } else if (os.type("unix") & Sys.which("wget") != "") {
-      download.file(url, destfile = tmp, quiet = TRUE, method = "wget")
-    } else if (os.type("unix") & Sys.which("curl") != "") {
-      download.file(url, destfile = tmp, quiet = TRUE, method = "curl")
-    } else {
-      127
-    }
-
-  # check return value
-  if (ret == 0) {
-    # scan lines
-    scan(tmp, what = "", sep = "\n", quiet = TRUE)
-  } else {
-    message("Unable to download file so using slower method url().\n",
-            "Try setting an appropriate value via \n\toptions(download.file.method = ...)\n",
-            "see ?download.file for more information.")
-    # connect to url
-    con <- url(url)
-    on.exit(close(con))
-
-    # scan lines
-    scan(con, what = "", sep = "\n", quiet = TRUE)
+  # read url contents
+  resp <- httr::GET(url)
+  if (httr::http_type(resp) != "text/xml") {
+    stop("API did not return xml", call. = FALSE)
   }
+
+  # return as list
+  resp <-httr::content(resp)
+  xml2::as_list(resp)
 }
 
 
-
 parseSAG <- function(x) {
-  # simply parse using line and column separators
-  type <- gsub("*<ArrayOf(.*?) .*", "\\1", x[2])
-  starts <- grep(paste0("<", type, ">"), x)
-  ends <- grep(paste0("</", type, ">"), x)
-  ncol <- unique(ends[1] - starts[1]) - 1
-  # drop everything we don't need
-  x <- x[-c(1,2, starts, ends, length(x))]
+  # assume x is a table structure
+  xrow <- structure(rep(NA, length(x[[1]])), names = names(x[[1]]))
+  x <- lapply(unname(x), unlist)
+  # add NAs to empty columns
+  bool <- sapply(x, length) < length(xrow)
+  x[bool] <- lapply(x[bool], function(y) {out <- xrow; out[names(y)] <- y; out})
+  # rbind into a matrix
+  x <- do.call(rbind, x)
 
-  # exit if no data is being returned
-  if (length(x) == 0) return(NULL)
-
-  # match content of first <tag>
-  names_x <- gsub(" *<(.*?)(>| />).*", "\\1", x[1:ncol])
-
-  # delete all <tags>
-  x <- gsub(" *<.*?>", "", x)
+  # remove any html tags
+  x <- gsub("<.*?>", "", x)
+  
   # trim white space
   x <- trimws(x)
-
+  
   # SAG uses "" and "NA" to indicate NA
   x[x == ""] <- NA
   x[x == "NA"] <- NA
-
+    
   # make into a data.frame
-  dim(x) <- c(ncol, length(x)/ncol)
-  row.names(x) <- names_x
-  x <- as.data.frame(t(x), stringsAsFactors = FALSE)
+  x <- as.data.frame(x, stringsAsFactors = FALSE)
+
 
   # simplify and return
   simplify(x)
@@ -75,69 +52,38 @@ parseSAG <- function(x) {
 
 parseSummary <- function(x) {
 
-  # check for not published:
-  if ( gsub(" *<.*?>", "", x[3]) == "Stock not published") {
-    return(NULL)
-  }
-  if (gsub(" *<.*?>", "", x[3]) != "Stock published") {
-    stop("Something went wrong")
-  }
-
-  # Extract table size
-  type <- "SummaryTableLine"
-  starts <- grep(paste0("<", type, ">"), x)
-  ends <- grep(paste0("</", type, ">"), x)
-  ncol <- unique(ends[1] - starts[1]) - 1
-
   # get auxilliary info
-  info <- x[3:(starts[1]-2)]
-  # match content of first <tag>
-  names_info <- gsub(" *<(.*?)>.*", "\\1", info)
-  # delete all <tags>
-  info <- gsub(" *<.*?>", "", info)
-  names(info) <- names_info
-
-  # read summary table
-  x <- x[-c(1:(starts[1]-1), starts, ends, length(x) + -1:0)]
-
-  # match content of first <tag>
-  names_x <- gsub(" *<(.*?)>.*", "\\1", x[1:ncol])
-  # also remove xsi:nil:
-  names_x <- gsub(" xsi:nil=\"true\" /", "", names_x)
-
-  # delete all <tags>
-  x <- gsub(" *<.*?>", "", x)
-
-  # make into a data.frame
-  dim(x) <- c(ncol, length(x)/ncol)
-  row.names(x) <- names_x
-  x <- as.data.frame(t(x), stringsAsFactors = FALSE)
-
-  # tag on info
-  x <- cbind(x, data.frame(t(info)))
-
+  info <- as.data.frame(lapply(x[names(x) != "lines"], "[[", 1), stringsAsFactors = FALSE)
   # tidy
   x[x == ""] <- NA
   x[x == "NA"] <- NA
-
   # simplify
-  simplify(x)
+  info <- simplify(info)
+
+  # check for not published:
+  if (info$StockPublishNote == "Stock not published") {
+    return(NULL)
+  }
+  if (info$StockPublishNote != "Stock published") {
+    stop("Something went wrong")
+  }
+
+  # parse summary table  
+  x <- parseSAG(x[["lines"]])
+  
+  # tag on info and return
+  cbind(x, info, stringsAsFactors = FALSE)
 }
 
 
-#' @importFrom png readPNG
 parseGraph <- function(x) {
 
-  fileurl <- gsub(" *<.*?>", "", x[2])
-
-  if (fileurl == "D:\\StandardGraphs\\Download\\notpublished.png") {
-    return(NULL)
-  }
+  fileurl <- unlist(x)
 
   tmp <- tempfile(fileext = ".png")
-  download.file(fileurl, tmp, mode="wb", quiet=TRUE)
+  utils::download.file(fileurl, tmp, mode="wb", quiet=TRUE)
 
-  out <- readPNG(tmp)
+  out <- png::readPNG(tmp)
 
   class(out) <- c("ices_standardgraph", class(out))
   out
@@ -145,9 +91,8 @@ parseGraph <- function(x) {
 
 
 #' @export
-#' @importFrom grid grid.raster
 plot.ices_standardgraph <- function(x, y = NULL, ...) {
-  grid.raster(x)
+  grid::grid.raster(x)
 }
 
 #' @export
@@ -163,7 +108,7 @@ plot.ices_standardgraph_list <- function(x, y = NULL, ...) {
   x_loc <- rep((1:r)/r - 1/(2*r), c)
   y_loc <- rep((c:1)/c  - 1/(2*c), each = r)
   for (i in seq_along(x)) {
-    grid.raster(x[[i]], x = x_loc[i], y = y_loc[i], width = 1/r, height = 1/c)
+    grid::grid.raster(x[[i]], x = x_loc[i], y = y_loc[i], width = 1/r, height = 1/c)
   }
 }
 
@@ -171,11 +116,12 @@ plot.ices_standardgraph_list <- function(x, y = NULL, ...) {
 
 checkSAGWebserviceOK <- function() {
   # return TRUE if web service is active, FALSE otherwise
-  out <- readSAG("https://sg.ices.dk/StandardGraphsWebServices.asmx/getSummaryTable?key=-1")
+  out <- httr::GET("https://sg.ices.dk/StandardGraphsWebServices.asmx/getSummaryTable?key=-1")
 
   # check server is not down by inspecting XML response for internal server error message
-  if(!grepl("SummaryTable", out[2])) {
-    warning("Web service failure: the server seems to be down, please try again later.")
+  if(httr::http_error(out)) {
+    warning("Web service failure: the server seems to be down, please try again later.\n",
+            "http status message: ", httr::http_status(out)$message)
     FALSE
   } else {
     TRUE
@@ -183,7 +129,6 @@ checkSAGWebserviceOK <- function() {
 }
 
 
-#' @importFrom utils capture.output
 checkYearOK <- function(year) {
   # check year against available years
   all_years <- getListStocks(year = 0)
@@ -191,7 +136,7 @@ checkYearOK <- function(year) {
 
   if (!year %in% published_years) {
     message("Supplied year (", year, ") is not available.\n  Available options are:\n",
-            paste(capture.output(print(sort(published_years))), collapse = "\n"))
+            paste(utils::capture.output(print(sort(published_years))), collapse = "\n"))
     FALSE
   } else {
     TRUE
