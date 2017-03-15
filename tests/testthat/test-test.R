47ce3b386d3cab99f442d2b09c6d3672fa8cb3de
colin millar <colinpmillar@gmail.com>
2017-03-15 12:19:49 +0100

    add tests for SAG upload functions

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,5 +1,14 @@
 context('webservice access')
 
 test_that('Webservice access is okay', {
-  expect_equal(checkSAGWebserviceOK(), TRUE)
+  expect_equal(sag_checkWebserviceOK(), TRUE)
+})
+
+
+test_that('SAG upload', {
+  stk <- readSAGUpload("http://ecosystemdata.ices.dk/download/whb_comb_1999.xml")
+  stkxml <- writeSAGUpload(stk$info, stk$fishdata)
+  expect_equal(stk, readSAGUpload(stkxml))
+  stk <- readSAGUpload(stkxml)
+  expect_equal(stkxml, writeSAGUpload(stk$info, stk$fishdata))
 })
