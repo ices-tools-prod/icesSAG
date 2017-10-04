52a53fe2263252c90c32d869b51a92b34cf9736d
Arni Magnusson <arnima@hafro.is>
2017-10-04 13:17:42 +0200

    databse -> database

--- R/getStockDownloadData.R
+++ R/getStockDownloadData.R
@@ -1,13 +1,11 @@
-
-
 getStockDownloadData <- function(assessmentKey, ...) {
   warning("Use with caution!\n",
           "  getStockDownloadData accesses a table that is updated every 12 hours and may\n",
-          "  not reflect the current state of the data held in the databse.")
+          "  not reflect the current state of the database.")
 
   assessmentKey <- checkKeyArg(assessmentKey = assessmentKey, ...)
 
-  # call webservice for all supplied keys
+  # call web service for all supplied keys
   out <- lapply(assessmentKey, function(i) sag_webservice("getStockDownloadData", assessmentKey = i))
 
   # parse output
