a2aefd6304698ae621800d9eeeb899497b15fca3
Colin Millar <colin@ices.dk>
2018-06-08 12:33:37 +0200

    adding getStockSourceData

--- R/getStockDownloadData.R
+++ R/getStockDownloadData.R
@@ -22,8 +22,12 @@
 #' sourcedat <- getStockDownloadData(assessmentKey)
 #' head(sourcedat[[1]])
 #'
-#' @export
+#' @rdname getStockDownloadData
+#' @name getStockSourceData
+NULL
 
+#' @rdname getStockDownloadData
+#' @export
 getStockDownloadData <- function(assessmentKey, ...) {
   assessmentKey <- checkKeyArg(assessmentKey = assessmentKey, ...)
 
@@ -32,4 +36,25 @@ getStockDownloadData <- function(assessmentKey, ...) {
 
   # parse output
   lapply(out, sag_parse, type = "table")
+}
+
+#' @rdname getStockDownloadData
+#' @export
+getStockSourceData <- function(assessmentKey, ...) {
+  assessmentKey <- checkKeyArg(assessmentKey = assessmentKey, ...)
+
+  # get uri all supplied keys
+  out <- sapply(assessmentKey, function(i) sag_documentService_uri("XMLDownload", assessmentKey = i))
+
+  # read and parse output
+  out <- lapply(out, function(x) {
+    message("Reading, ", x)
+    readSAGxml(x)
+    })
+
+  # add stock code names
+  names(out) <- sapply(out, function(x) paste0(x$info$StockCode, "_", x$info$AssessmentYear))
+
+  # return
+  out
 }
