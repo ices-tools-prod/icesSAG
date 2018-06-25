05ac36fe9a9c55225b2c2489956d10f35d421786
Colin Millar <colinpmillar@gmail.com>
2018-06-25 16:19:32 +0200

    Fix get source xml data function
    still a hack though - need to develop a better API that returns 404 / 403 or 200

--- R/getStockDownloadData.R
+++ R/getStockDownloadData.R
@@ -43,17 +43,32 @@ getStockDownloadData <- function(assessmentKey, ...) {
 getStockSourceData <- function(assessmentKey, ...) {
   assessmentKey <- checkKeyArg(assessmentKey = assessmentKey, ...)
 
+  # must use token for this service
+  # check that token is set
+  opts <- options(icesSAG.use_token = TRUE)
+  on.exit(options(opts))
+
   # get uri all supplied keys
   out <- sapply(assessmentKey, function(i) sag_documentService_uri("XMLDownload", assessmentKey = i))
 
   # read and parse output
   out <- lapply(out, function(x) {
     message("Reading, ", x)
-    readSAGxml(x)
-    })
+    # peek first
+    peek <- suppressWarnings(readLines(x))
+    if (length(peek) == 4 || peek[1] %in% c("Sorry stock not found", "Not autorized, please specify the token")) {
+      return(NULL)
+    } else {
+      readSAGxml(x)
+    }
+  })
 
   # add stock code names
-  names(out) <- sapply(out, function(x) paste0(x$info$StockCode, "_", x$info$AssessmentYear))
+  notnull <- !sapply(out, is.null)
+  if (any(notnull)) {
+    names(out)[notnull] <- sapply(out[notnull], function(x) paste0(x$info$StockCode, "_", x$info$AssessmentYear))
+  }
+
 
   # return
   out
