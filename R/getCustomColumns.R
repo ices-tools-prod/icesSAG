cf5585d744219784f15eb0fd2514163331a81451
Colin Millar <colin.millar@ices.dk>
2019-11-18 13:49:00 +0100

    fixes after linting

--- R/getCustomColumns.R
+++ R/getCustomColumns.R
@@ -1,6 +1,6 @@
 #' Get the Custom Columns for SAG records
 #'
-#' Get custom columns, such as alternative biomass series or Fproxy 
+#' Get custom columns, such as alternative biomass series or Fproxy
 #'   reference points for a record in the SAG database.
 #'
 #' @param assessmentKey the unique identifier of the stock assessment
@@ -30,23 +30,23 @@
 getCustomColumns <- function(assessmentKey, ...) {
 
   df <- getStockDownloadData(assessmentKey, ...)
-  
+
   do.call(rbind,
     lapply(df,
       function(x) {
           id <- grep("CustomName*", names(x))
-    
+
           if (!length(id)) {
             return(NULL)
           }
 
           # get stock level stuff
-          out <- as.list(x[1,c("AssessmentKey", "AssessmentYear", "StockKeyLabel", "Purpose")])
+          out <- as.list(x[1, c("AssessmentKey", "AssessmentYear", "StockKeyLabel", "Purpose")])
                   
           customIds <- as.numeric(gsub("CustomName", "", names(x)[id]))
-          
+
           customs <- 
-            do.call(rbind, 
+            do.call(rbind,
               lapply(customIds, function(i) {
                 data.frame(
                   Year = x$Year,
