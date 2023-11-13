c840c6680ff719c669dd8de3cd4a03cf546f4712
Colin Millar <colin.millar@ices.dk>
2023-11-13 14:28:05 +0100

    add new api route

---
+++ R/httpUtils.R
@@ -0,0 +1,35 @@
+api_url <- function() {
+  "https://sag.ices.dk/SAG_API/api"
+}
+
+#' @importFrom icesConnect ices_get_jwt
+#' @importFrom httr content
+ices_get <- function(url, retry = TRUE, quiet = FALSE, verbose = FALSE, content = TRUE, use_token = FALSE) {
+  resp <-
+    ices_get_jwt(
+      url,
+      retry = retry, quiet = quiet, verbose = verbose,
+      jwt = if (use_token) NULL else ""
+    )
+
+  if (content) {
+    content(resp, simplifyVector = TRUE)
+  } else {
+    resp
+  }
+}
+
+#' @importFrom icesConnect ices_post_jwt
+ices_post <- function(url, body = list(), retry = TRUE, verbose = FALSE, use_token = FALSE) {
+  out <-
+    ices_post_jwt(
+      url,
+      body,
+      encode = "multipart",
+      retry = retry,
+      verbose = verbose,
+      jwt = if (use_token) NULL else ""
+    )
+
+  return(out)
+}
